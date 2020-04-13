5885c270472dd5b87c69b0f3f0e4d440f4c47f98
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2020-04-14 03:53:04 +0530

    Added files via upload
    Free to access, but give credit.

---
+++ Generic Sorting Algorithms/Merge Sort/Source.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<conio.h>
+#include<vector>
+
+void mergesort(std::vector<int>&,int,int);
+void merge(int, int, int, std::vector<int>& );
+
+int main() {
+	int size,val;
+	std::vector<int> vec;
+	std::cout << "Enter the number of elements to be sorted" << std::endl;
+	std::cin >> size;
+	std::cout << "Enter the values" << std::endl;
+	for (int i = 0;i < size;i++) {
+		std::cin >> val;
+		vec.push_back(val);
+	}
+	mergesort(vec, 0, size - 1);
+	for (int i = 0;i < size;i++) {
+		std::cout << vec[i]<<" ";
+	}
+	_getch();
+}
+void merge(int start, int mid , int end,std::vector<int>& vec) {
+	std::vector<int> left;
+	std::vector<int> right;
+	for (int i = start;i <= mid;i++) {
+		left.push_back(vec[i]);
+	}
+	for (int i = mid+1 ;i <=end ;i++) {
+		right.push_back(vec[i]);
+	}
+	int i = 0,k=start,j = 0;
+	while (i != mid-start+1 && j != end-mid) {
+	if (left[i] <= right[j]) {
+			vec[k] = left[i];
+			i++; k++;
+		}
+		else{
+			vec[k] = right[j];
+			j++;k++;
+		}
+	}
+	if (i == mid - start + 1) {
+		for (;k <= end;k++) { vec[k] = right[j]; j++; }
+	}
+	else {
+		for (;k <=end ;k++) { vec[k] = left[i]; i++; }
+	}
+}
+void mergesort(std::vector<int>& vec,int start,int end) {
+	if (end > start) {
+		int mid = (start + end) / 2;
+		mergesort(vec , start, mid);
+		mergesort(vec, mid + 1,end);
+		merge(start, mid, end, vec);
+	}
+}
