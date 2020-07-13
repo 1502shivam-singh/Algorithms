86c19e46e818aeec79710e65c4b5816742ece903
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2020-07-13 21:24:50 +0530

    Added some comments for understanding

--- Searching algorithms/Binary search/Source.cpp
+++ Searching algorithms/Binary search/Source.cpp
@@ -8,7 +8,8 @@ int Binary_Search(const std::vector<int>&, int, int, int);  //Function declartio
 
 int main() {
 	std::vector<int> vector{ 2,3,4,5,6,7,8,9,10,11,12,34,556,780,1000,1001,2000,3000,4000,5000,6000,7000,8000,9000};	//input array must be sorted for the algorithm to work
-	std::cout << Binary_Search(vector, 8, 0, vector.size() - 1);
+	std::cout << Binary_Search(vector, 8, 0, vector.size() - 1);	
+//function calling sequence (vector storing values, target, start index of section of vector in which you have to search, end index of section of vector in which you have to search)
 	_getch();
 }
 
